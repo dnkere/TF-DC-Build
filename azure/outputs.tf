c36ca9198525f4c9b963f2ca9eeba7488a6f7ca8
ollion_daiquan <151578817+dnkere@users.noreply.github.com>
2023-12-21 12:02:22 -0600

    Delete azure directory

--- azure/outputs.tf
+++
@@ -1,5 +0,0 @@
-# API Outputs
-output "slash_echo_url" {
-    value       = "${azurerm_function_app.slash_echo.default_hostname}/api/${azurerm_function_app.slash_echo.name}"
-    description = "The URL that gets configured into Slack to send the POST."
-}
