8a072534953ac9de8b00832193bde39d9e39f984
ollion_daiquan <151578817+dnkere@users.noreply.github.com>
2023-12-21 12:02:01 -0600

    Delete aws directory

--- aws/outputs.tf
+++
@@ -1,5 +0,0 @@
-# API Outputs
-output "slash_echo_url" {
-    value = "${aws_api_gateway_deployment.slash_echo_deployment.invoke_url}/${aws_api_gateway_resource.echo.path_part}"
-    description = "The URL that gets configured into Slack to send the POST."
-}
